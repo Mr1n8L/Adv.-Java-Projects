df1062b7fb9a46acb221c5e49c2054992cc68821
Mr1n8L <mrinaljani1822@gmail.com>
2024-07-04 22:37:22 +0530

    Update N-Queens.java

--- N-Queens.java
+++ N-Queens.java
@@ -52,4 +52,30 @@ public void saveBoard(char[][] board, List<List<String>> allBoards) {
     String row = "";
     List<String> newBoard = new ArrayList<>();
    
-    for(i
+    for(int i=0; i<board.length; i++) {
+        row = "";
+        for(int j=0; j<board[0].length; j++) {
+            if(board[i][j] == 'Q')
+                row += 'Q';
+            else
+                row += '.';
+        }
+        newBoard.add(row);
+    }
+   
+    allBoards.add(newBoard);
+}
+
+public void helper(char[][] board, List<List<String>> allBoards, int col) {
+    if(col == board.length) {
+        saveBoard(board, allBoards);
+        return;
+    }
+   
+    for(int row=0; row<board.length; row++) {
+        if(isSafe(row, col, board)) {
+            board[row][col] = 'Q';
+            helper(board, allBoards, col+1);
+            board[row][col] = '.';
+        }
+    }
