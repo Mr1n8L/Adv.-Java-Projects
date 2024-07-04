a76ba7e88db144efe12e893661bce9bf4b6cfe9c
Mr1n8L <mrinaljani1822@gmail.com>
2024-07-04 22:08:50 +0530

    Update N-Queens.java

--- N-Queens.java
+++ N-Queens.java
@@ -1,3 +1,10 @@
 class Solution {
     public boolean isSafe(int row, int col, char[][] board) {
         //horizontal
+        for(int j=0; j<board.length; j++) {
+            if(board[row][j] == 'Q') {
+                return false;
+            }
+        }
+       
+ 
