19fb57479e992580d435be10b9ad612cd0b9165f
Mr1n8L <mrinaljani1822@gmail.com>
2024-07-07 00:30:53 +0530

    Update SudokuSolver.java

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,5 +1,20 @@
 public class SudokuSolver {
+    public boolean helper(char board[][], int row, int col){
+
+        int newRow = 0;
+        int newCol = 0;
+        if(col != board.length - 1){
+            newRow = row;
+            newCol = col + 1;
+        }
+        else{
+            newRow = row + 1;
+            newCol = 0;
+        }
+
+    }
     public void solveSudoku(char board[][]){
-        
+        helper(board, 0, 0)
+
     }
 }
